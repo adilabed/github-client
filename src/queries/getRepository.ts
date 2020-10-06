6be77c3c9c2326977f09369fed7dd1ca6fca5428
Adil <aelabedelalaoui@acm.org>
2020-10-06 13:56:34 +0200

    Create new issues

---
+++ src/queries/getRepository.ts
@@ -0,0 +1,8 @@
+import { gql } from "apollo-boost";
+export const GET_REPOSITORY = gql`
+  query getRepository($owner: String!, $name: String!) {
+    repository(owner: $owner, name: $name) {
+      id
+    }
+  }
+`
